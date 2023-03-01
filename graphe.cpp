d1617230c5f0325db332f220f8405fc39f30c2d0
Nedjma <bouamaranedjma@gmail.com>
2023-03-01 12:51:13 +0100

    Ajout d'une fonction la fonction List_sommets() qui retourne la liste des sommets du graphe

--- graphe.cpp
+++ graphe.cpp
@@ -1,7 +1,19 @@
 #include "graphe.h"
 
-// lists des voisins d'un noeud
-vector<string> Graphe::List_voisins(string sommet){
+//---------- la liste des sommets du graphe
+vector<string> Graphe::List_sommets()
+{
+    vector<string> list_sommet;
+    for (auto iter = graphe.begin(); iter != graphe.end(); ++iter)
+    {
+        list_sommet.push_back((*iter).first);
+    }
+    return list_sommet;
+}
+
+//----------- lists des voisins d'un noeud
+vector<string> Graphe::List_voisins(string sommet)
+{
     vector<string> list = {};
     auto voisins = graphe.find(sommet);
     if (voisins != graphe.end())
@@ -14,3 +26,5 @@ vector<string> Graphe::List_voisins(string sommet){
 
     return list;
 }
+
+
--- graphe.h
+++ graphe.h
@@ -9,6 +9,10 @@ struct Graphe
     // table de hashage qui va stocker le graphe
     unordered_map<string, vector<string>> graphe;
 
+    // retourner la liste des sommets du graphe
+    vector<string> List_sommets();
+
     // retourne la liste des voisins d'un sommet donné
     vector<string> List_voisins(string sommet);
+
 };
